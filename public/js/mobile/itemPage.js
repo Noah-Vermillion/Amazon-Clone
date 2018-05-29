38d39e729005ba6e17e554f24590d0965f5fc6a0
Megapixel99 <wheelersc@cox.net>
2018-05-28 17:15:11 -0700

    revert

--- public/js/mobile/itemPage.js
+++ public/js/mobile/itemPage.js
@@ -55,7 +55,7 @@ $(document).ready(function() {
   $.get("/userInfo", function(data) {
     if (data != null) {
       if (data.username) {
-        $("#username").attr('href', "../../../account");
+        $("#username").attr('href', "../../account");
         $("#username").html(data.username);
       }
     }
